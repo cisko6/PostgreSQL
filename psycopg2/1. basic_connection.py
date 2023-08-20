542d99625daf8fdbd07ea9542e80fc19a0ffd04b
superc1so <patrikcisar1999@gmail.com>
2023-08-20 16:38:09 +0200

    Update 1. basic_connection.py

--- psycopg2/1. basic_connection.py
+++ psycopg2/1. basic_connection.py
@@ -1,12 +1,14 @@
 
 import psycopg2
 
+vysl = input("Zadaj heslo: ")
+
 connection = psycopg2.connect(
     host="localhost",
     database="postgres",
     port=5432,
     user="postgres",
-    password="cisco"
+    password=vysl
 )
 
 cursor = connection.cursor()
